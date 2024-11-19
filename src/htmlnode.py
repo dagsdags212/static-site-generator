2c64fc9e97c12f54b79c6a93bbdae72d369f3921
dagsdags212 <jegsamson.dev@gmail.com>
2024-11-19 14:13:17 +0800

    added ParentNode class and related tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional
+from typing import Optional, Iterable
 
 
 class HTMLNode:
@@ -7,7 +7,7 @@ class HTMLNode:
     def __init__(self, 
                  tag: Optional[str]=None, 
                  value: Optional[str]=None, 
-                 children: Optional[list[HTMLNode]]=None,
+                 children: Optional[Iterable[HTMLNode]]=None,
                  props: Optional[dict[str, str]]=None):
         self.tag = tag
         self.value = value
@@ -17,10 +17,12 @@ class HTMLNode:
     def to_html(self):
         raise NotImplemented
 
-    def props_to_html(self) -> Optional[str]:
+    def props_to_html(self) -> str:
         """Returns a string that represents HTML attributes of a node."""
         if self.props:
-            return " ".join([f'{attr}="{val}"' for attr, val in self.props.items()])
+            s = "".join([f'{attr}="{val}"' for attr, val in self.props.items()])
+            return " " + s
+        return ""
 
     def __repr__(self) -> str:
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
@@ -31,12 +33,32 @@ class LeafNode(HTMLNode):
     def __init__(self, tag: Optional[str], value: str, props: Optional[dict[str, str]]=None) -> None:
         super().__init__(tag, value, None, props)
 
-    def to_html(self):
+    def to_html(self) -> str:
         if self.value is None:
             raise ValueError("missing value")
         if self.tag is None:
             # return raw text
             return self.value
         # return a rendered HTML tag
-        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
+
+class ParentNode(HTMLNode):
+    def __init__(self, 
+                 tag: str, 
+                 children: Iterable[HTMLNode], 
+                 props: Optional[dict[str,str]]=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("missing tag")
+        if len(self.children) == 0:
+            raise ValueError("must have at least one child node")
+
+        html = f"<{self.tag}{self.props_to_html()}>"
+        # recursively add children node
+        for child in self.children:
+            html += child.to_html()
+        html += f"</{self.tag}>"
+        return html
---
+++ src/test_parentnode.py
@@ -0,0 +1,51 @@
+import unittest
+from htmlnode import LeafNode, ParentNode
+
+
+class TestParentNode(unittest.TestCase):
+    def test_node_without_tag(self):
+        children = \
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ]
+        with self.assertRaises((AttributeError, TypeError)):
+            ParentNode(children=children)
+
+    def test_node_without_children(self):
+        with self.assertRaises((AttributeError, TypeError)):
+            ParentNode(tag="div")
+
+
+
+    def test_to_html_method_with_two_levels(self):
+        parent = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],) 
+        html = parent.to_html()
+        expected = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+        self.assertEqual(html, expected)
+
+    def test_to_html_method_with_three_levels(self):
+        root = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode("i", "italic text"),
+                ParentNode("div",
+                    [LeafNode(None, "Normal text"),
+                     LeafNode("p", "Paragraph")]
+                ),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        html = root.to_html()
+        expected = "<p><b>Bold text</b><i>italic text</i><div>Normal text<p>Paragraph</p></div>Normal text</p>"
+        self.assertEqual(html, expected)
