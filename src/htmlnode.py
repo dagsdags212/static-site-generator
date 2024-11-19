fb3866ae597a078e26ce5cd8a3df5df7b4787310
dagsdags212 <jegsamson.dev@gmail.com>
2024-11-19 13:26:10 +0800

    added LeafNode child class and related tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,9 +20,23 @@ class HTMLNode:
     def props_to_html(self) -> Optional[str]:
         """Returns a string that represents HTML attributes of a node."""
         if self.props:
-            return " ".join([f"{attr}={val}" for attr, val in self.props.items()])
+            return " ".join([f'{attr}="{val}"' for attr, val in self.props.items()])
 
     def __repr__(self) -> str:
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
 
+
+class LeafNode(HTMLNode):
+    """HTMLNode with no children."""
+    def __init__(self, tag: Optional[str], value: str, props: Optional[dict[str, str]]=None) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("missing value")
+        if self.tag is None:
+            # return raw text
+            return self.value
+        # return a rendered HTML tag
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
 
---
+++ src/test_leafnode.py
@@ -0,0 +1,25 @@
+import unittest
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_node_has_no_children(self):
+        leaf = LeafNode("p", "This is a leaf node")
+        self.assertIsNone(leaf.children)
+
+    def test_node_without_value(self):
+        with self.assertRaises((TypeError, ValueError)):
+            leaf = LeafNode("a")
+
+    def test_to_html_method(self):
+        attributes = dict(value="Click me!", props={"href": "https://www.google.com"})
+        # node without tag should return raw value as text
+        leaf_without_tag = LeafNode(tag=None, **attributes)
+        self.assertEqual(leaf_without_tag.to_html(), leaf_without_tag.value)
+         
+        # node with tag should return a formatted HTML tag
+        leaf_with_tag = LeafNode(tag="a", **attributes)
+        self.assertIsInstance(leaf_with_tag.to_html(), str)
+        self.assertEqual(
+            leaf_with_tag.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
